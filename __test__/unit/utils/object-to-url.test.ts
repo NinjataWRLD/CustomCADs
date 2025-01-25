9e85e69716859df7ac765f16a928da307d7ede97
Ivan Angelov <ivanangelov414@gmail.com>
2025-01-25 23:34:08 +0200

    Set up Jest and unit tested utils

---
+++ __test__/unit/utils/object-to-url.test.ts
@@ -0,0 +1,39 @@
+import objectToUrl from '@/utils/object-to-url';
+
+describe('Object to URL utility function tests', () => {
+	it('should convert a simple object to a URL query string', () => {
+		const obj = { key1: 'value1', key2: 'value2' };
+		const url = objectToUrl(obj);
+		expect(url).toBe('key1=value1&key2=value2');
+	});
+
+	it('should handle empty objects', () => {
+		const obj = {};
+		const url = objectToUrl(obj);
+		expect(url).toBe('');
+	});
+
+	it('should encode special characters', () => {
+		const obj = { key: 'value with spaces', special: 'chars&symbols' };
+		const url = objectToUrl(obj);
+		expect(url).toBe('key=value%20with%20spaces&special=chars%26symbols');
+	});
+
+	it('should handle null and undefined values', () => {
+		const obj = { key1: null, key2: undefined, key3: 'value3' };
+		const url = objectToUrl(obj);
+		expect(url).toBe('key3=value3');
+	});
+
+	it('should handle boolean values', () => {
+		const obj = { key1: true, key2: false, key3: 'value3' };
+		const url = objectToUrl(obj);
+		expect(url).toBe('key1=true&key2=false&key3=value3');
+	});
+
+	it('should handle arrays as values', () => {
+		const obj = { key: ['value1', 'value2'] };
+		const url = objectToUrl(obj);
+		expect(url).toBe('key=value1%2Cvalue2');
+	});
+});
