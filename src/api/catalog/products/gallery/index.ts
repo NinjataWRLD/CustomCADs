781c996dadd4dfd215fca686f1d2260858e9a7dc
Ivan Angelov <ivanangelov414@gmail.com>
2025-01-30 16:20:48 +0200

    Added Download Image/Cad flows

--- src/api/catalog/products/gallery/index.ts
+++ src/api/catalog/products/gallery/index.ts
@@ -3,6 +3,7 @@ import { Result } from '@/api/common/result';
 import * as allResources from './resources/all';
 import * as singleResources from './resources/single';
 import * as sortingsResources from './resources/sortings';
+import * as downloadResources from './resources/download';
 
 export const all = async (req: allResources.Request) =>
 	await axios.get<Result<allResources.Response>>(allResources.url(req));
@@ -12,3 +13,15 @@ export const single = async (req: singleResources.Request) =>
 
 export const sortings = async () =>
 	await axios.get<sortingsResources.Response>(sortingsResources.url());
+
+export const downloadImage = async (req: downloadResources.Request) =>
+	await axios.post<downloadResources.Response>(
+		downloadResources.url('image'),
+		req,
+	);
+
+export const downloadCad = async (req: downloadResources.Request) =>
+	await axios.post<downloadResources.Response>(
+		downloadResources.url('cad'),
+		req,
+	);
