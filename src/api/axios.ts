5352e63e70a5d987c9ab8e753cf4caf5c906b133
Ivan Angelov <ivanangelov414@gmail.com>
2025-04-10 01:01:03 +0300

    Refactored axios logic

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,6 @@
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
+import * as authStore from '@/stores/auth-store';
+import { authz, refresh } from './identity/identity';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 const API_VERSION = import.meta.env.VITE_API_VERSION ?? 'v1';
@@ -10,5 +12,26 @@ const instance = axios.create({
 		'Content-Type': 'application/json',
 	},
 });
+
+instance.interceptors.response.use(
+	(response) => response,
+	async (error) => {
+		if (
+			!(isAxiosError(error) && error.response?.status === 401) ||
+			error.config?.url === '/identity/signin/refresh'
+		)
+			return Promise.reject(error);
+
+		try {
+			await refresh();
+			const { data: role } = await authz();
+
+			authStore.login(role);
+			return axios(error.config!);
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	},
+);
 
 export { instance as axios };
--- src/hooks/useCartItemManager.ts
+++ src/hooks/useCartItemManager.ts
@@ -1,30 +1,28 @@
+import { isAxiosError } from 'axios';
 import { useEffect } from 'react';
 import { useCreateCustomization } from '@/hooks/mutations/customizations';
 import { useGetCustomization } from '@/hooks/queries/customizations';
 import { useCartUpdates } from '@/hooks/contexts/useCartUpdates';
 import { useCartContext } from '@/hooks/contexts/useCartContext';
-import { isAxiosError } from '@/utils/api';
 
 export const useCartItemManager = (productId: string) => {
 	const { items } = useCartContext();
 	const item = items?.find((i) => i.productId === productId);
 
-	const { mutateAsync: createCustomization, data: createCustomizationData } =
-		useCreateCustomization();
+	const { mutateAsync: create, data: createData } = useCreateCustomization();
 
 	const customizationId =
-		createCustomizationData?.id ??
-		(item?.forDelivery ? item.customizationId : null);
+		createData?.id ?? (item?.forDelivery ? item.customizationId : null);
 
-	const { data: customization, error: customizationError } =
-		useGetCustomization({ id: customizationId! }, !!customizationId);
+	const { data: customization, error: error } = useGetCustomization(
+		{ id: customizationId! },
+		!!customizationId,
+	);
 
 	useEffect(() => {
-		if (
-			items &&
-			(!customizationId || isAxiosError(customizationError, 404))
-		) {
-			createCustomization({
+		const notFound = isAxiosError(error) && error.status === 404;
+		if (items && (!customizationId || notFound)) {
+			create({
 				materialId: 1,
 				color: '#ffffff',
 				infill: 0.2,
@@ -32,7 +30,7 @@ export const useCartItemManager = (productId: string) => {
 				volume: 0,
 			});
 		}
-	}, [items, customizationId, customizationError]);
+	}, [items, customizationId, error]);
 
 	const { addItem } = useCartUpdates();
 	useEffect(() => {
